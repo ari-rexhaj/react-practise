2aa344ff0a983c36278982411a83284f054afb31
Ari Rexhaj <arirexhajj@gmail.com>
2024-04-23 14:12:28 +0200

    backend mostly finished

---
+++ backend/index.js
@@ -0,0 +1,213 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const {Client} = require("pg");
+const app = express();
+
+const port = process.env.PORT || 3006;
+
+app.use(bodyParser.json());
+
+const client = new Client({
+    host: "dpg-coj187gl5elc73dffv70-a.frankfurt-postgres.render.com",
+    user: "ari_db_user",
+    port: "5432",
+    password: "uvnjcba54RGfTnu9xPyX2qiS1X2qdrhv",
+    database: "ari_db",
+    ssl: true
+});
+
+client.connect();
+app.listen(port, () =>  {
+    console.log(`Attempting startup at port ${port}`);
+});
+
+function isNumber(x, noStr) {
+    return (
+        (typeof x === 'number' || x instanceof Number || (!noStr && x && typeof x === 'string' && !isNaN(x))) &&
+        isFinite(x)
+    ) || false;
+};
+
+//APIs need to be foolproof because I HOLD MYSELF TO HIGH STANDARDS (idk if its foolproof)
+// if you can SQL inject than thats just a L, i know what it is, i dont know how to do anything about it,
+// and ill probably learn how to not get fucked later, for now it does not matter
+
+app.get("/Status", async (req,res) => {
+    console.log("Status called")
+    res.send({"status":"GOOD"})
+})
+
+//make a get posts (GET)
+app.get("/getPosts", async (req,res) => {
+    console.log("getPosts called")
+    client.query("TABLE posts", (error, response) => {
+        if (!error) {
+            console.log("getPosts success")
+            res.send(response.rows)
+        }
+        else {
+            console.log("getPosts fail")
+            res.status(500).json({ success: false, message: "Error getting posts" });
+        }
+    })
+});
+
+//not case sensitive
+//lets you search for text (really only title and tags in this case) where
+// you will by default search for titles and can choose to include tags or
+//not with the 'includetags' bool
+app.get("/searchPostsByText/:includetags", async (req,res) => {
+    console.log("searchPostsByText called")
+    let substring = req.body["search"].toLowerCase();
+    let includetags = req.params.includetags
+
+    client.query('SELECT * FROM posts', (error,result) => {
+        if (!error) {
+            let matchList = []; //list of matches
+            let postList = result.rows;
+            
+            for (let i = 0; i < postList.length; i++) {
+                if (postList[i]["title"].toLowerCase().includes(substring) || (includetags === "true" && postList[i]["tag"].toLowerCase() == substring)) {
+                    matchList.push(postList[i])
+                }
+            }
+            
+            console.log("searchPostsByText success");
+            res.send(matchList)
+        }
+        else {
+            console.log("searchPostsByText failed")
+            res.json({success:false,message:"Error searching for posts"})
+        }
+    })
+});
+
+//adds a post to the database, I think if you do not format the request body
+//correctly you will end up with undefined, soooooooo.... DO IT RIGHT
+app.post("/addPost", async (req,res) => {
+    console.log("addPost called");
+    //preparing the variables
+    let data = req.body;
+    let date = data["date"]
+    let tag = data["tag"]
+    let title = data["title"]
+    client.query('INSERT INTO posts (date, tag, title) VALUES ($1, $2, $3) RETURNING id', [date, tag, title], (error, result) => {
+        if (!error) {
+            console.log("addPost success");
+            const insertedId = result.rows[0].id;
+            res.json({ success: true, message: "Post added successfully", insertedId });
+        } else {
+            console.log("addPost failed at inserting new post");
+            res.status(500).json({ success: false, message: "Error adding post" });
+        }
+    });
+})
+
+//make a get posts for given type and index (GET)
+//to explain it nice and short, the query doesnt want quatation marks 
+//when refering to a column (can be thought of as key aswell) in the database, 
+//(i.e tag,date,id,title), but it does want the quotation marks when refering to
+//the index (ie. the value in the key-value pair), so ive written it to add the
+//type value into the string with + to remove the qoutation marks, and when 
+//adding the index i do it using the $ to add in the quotation marks. Which works
+//for seemingly all cases
+app.get("/getSpecificPosts/:type/:index", async (req,res) => {
+    console.log("getSpecificPosts called");
+    let type = req.params.type
+    let index = req.params.index
+        
+    // if you are wondering whats going on under this comment, let me break it down: https://i.pinimg.com/originals/7a/56/4c/7a564cac06f9b1a48491d8c72b10b0fd.gif
+    client.query('SELECT * FROM posts WHERE '+type+' = $1',[index],(error,result) => {
+        if(!error && result.rows.length !== 0) {
+            res.send(result.rows);
+            console.log("getSpecificPosts success")
+        }
+        else {
+            res.status(500).json({ success: false, message: "error getting filtered posts, did you correctly address the API?" });
+            console.log("getSpecificPosts failed")
+        }
+    })
+})
+
+//make a update entire post (PUT)
+//dont know why but this feels like a bad way to do it, but it does work. 
+//Maybe its because theres no checks making sure the formatting is correct, 
+//the API plugs the values in without thought.
+app.put("/updatePost/:id", async (req,res) => {
+    console.log("updatePost called");
+    //saves the different variables, this is very unstable since you can write whatever you want as the value to these things
+    let data = req.body
+    let date = data["date"]
+    let tag = data["tag"]
+    let title = data["title"]
+    let id = req.params.id;
+
+    if (data == undefined || date == undefined || tag == undefined || title == undefined) {
+        console.log("updatePost failed due to undefined data, did you format the request correctly?")
+        res.status(500).json({ success: false, message: "error updating post, did you format the body correctly? Remember to define a date, tag and title" });
+        return
+    }
+
+    client.query('UPDATE posts SET date = $1, tag = $2, title = $3 WHERE id = $4',[date,tag,title,id], (error, result) => {
+        if (!error) {
+            console.log("updatePost success")
+            res.json({ success: true, message: "Post updated successfully"});
+        }
+        else {
+            console.log("updatePost failed, data received below\n",data)
+            res.status(500).json({ success: false, message: "error updating post" });
+        }
+    })
+
+});
+
+//make a update value of post variable (PATCH)
+//works pretty well, will only update the given tag and index, does not 
+//support multiple values atm, because im not THAT good yet
+app.patch("/updatePostValue/:id", async (req,res) => {
+    console.log("updatePostValue called");
+    let id = req.params.id;
+    let data = JSON.stringify(req.body);
+    data = data.replace("{","").replace("}","").replaceAll('"','').split(":")   //some data parcing, converts the json into an array with the tag and the index
+
+    //querying, doing it the same way as always although im not sure if its neccessary here. BUT IF IT WORKS; DONT TOUCH IT!
+    //we are saying update the given Key-value (where data[0] refers to the key) with the given index (where data[1] refers to the value) for post with the given id, incase youre curious
+    client.query('UPDATE posts SET '+data[0]+' = $1 WHERE id = $2',[data[1],id], (error,result) => {
+        if (!error) {
+            console.log("updatePostValue success")
+            res.json({ success: true, message: "Post value updated successfully"});
+        }
+        else {
+            console.log("updatePostValue failed, data received below\n",data)
+            res.status(500).json({ success: false, message: "error updating post value" });
+        }
+    })
+});
+
+//WORKS, but deleting a post will make that a hole in the id counting as 
+//that post id will never be used again thanks to earlier systems, although 
+//this is not a big deal, my OCD does not enjoy this
+app.delete("/deletePost/:id", async (req,res) => {
+    console.log("deletePost called")
+    let id = req.params.id
+
+    if (!isNumber(id)) {
+        console.log("deletePost failed, id was not a number")
+        return
+    }
+
+    client.query('DELETE FROM posts WHERE id = $1',[id], (error,result) => {
+        if (!error) {
+            console.log("deletePost success")
+            res.json({success:true,message:"If post with given id existed, it was deleted successfully"})
+        }
+        else {
+            console.log("deletePost failed")
+            res.status(500).json({success:false,message:"Failed to delete post"})
+        }
+    })
+})
+
+// write more as you come up with them
+
+//optional: sort by date
--- react-practise/src/links/Home.js
+++ react-practise/src/links/Home.js
@@ -56,45 +56,19 @@ function postgenerator(metadatalist, filter) {
   //if you dont understand the code, let me break it down. Click the link below for starters
   //https://www.lovethispic.com/uploaded_images/38069-Breakdance-Spin-Gif.gif
   
-  //this function works by taking a list of metadata and using it to generate the components
-  //once the components are finished we pack them in a list and return them. Making it very easy
-  //to implement the entire wall of posts, basically anywhere you would like
-
-  //check if the filter is set to all, if not, we filter it and save it on the same variable.
-  //The reason we save it on the same variable is because this makes it so that if the list does 
-  //not need to be filtered, we can just continue with the inputed list, as that list has all 
-  //the blogpost metadata already
   if (filter !== "all") {
     metadatalist = metadatalist.filter((metadata) => metadata[1] === filter)
   }
-  
-  //here we create the components and use the information from the metadata. Since this is a test
-  //we only have date, tag and title. But for a real project this method would work for any data
-  //you would like to implement into a component, so long as the component is configured to use
-  //the data (which it obviously would be)
   const componentList = metadatalist.map((metadata) => <Blogpost date={metadata[0]} tag={metadata[1]} title={metadata[2]} />)
-  
-  //here we create an outerloop where we create the rows that the blogposts will sit inside
-  //we do some math here to figure out how many rows we need. It is made so that if you have
-  //for example 3 blogposts, it will only generate 1 row, but if you have 4 blogposts, it will 
-  //generate 2 rows. This is to gurantee that the blogposts do not overflow off the screen
   let rowList = [];
   for (let i = 0; i < Math.ceil(metadatalist.length/3); i++) {
 
-    //here we have an innerloop where we push in 3 blogposts into a list called RowItems. This
-    //list is later pushed into the list rowList, in short this is for correctly layering the
-    //blogposts into the rows. When we push a blogpost into rowItems, we use the .pop() method
-    //as this removes the blogpost aquired from the componentList, this means we gurantee that 
-    //we do not accidently push the same blogpost twice.
     let rowItems = [];  
     for (let i = 0; i < 3; i++) {
       rowItems.push(componentList.pop())
     }
 
-    //here we push the rowItems list into a row, which we then push into the rowList list
     rowList.push(<div className="row">{rowItems}</div>);
   }
-  //finally, after all the parsing and structuring is done, we return the rowList, which is ready
-  //to be placed anywhere in a react function
   return rowList
 }
